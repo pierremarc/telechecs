2a4606440af01068c04bf7d58f2276b81f538bb7
Pierre Marchand <pierre.m@atelier-cartographique.be>
2025-06-09 23:15:49 +0200

    forgot file

---
+++ src/view/follow.ts
@@ -0,0 +1,36 @@
+import { streamFollowing } from "../api";
+import { events } from "../lib/dom";
+import { DIV, replaceNodeContent } from "../lib/html";
+import { User } from "../lib/ucui/lichess-types";
+import { assign, dispatch, get, subscribe } from "../store";
+
+const getFollowing = () => {
+  streamFollowing((user) => {
+    dispatch("lichess/following", (fs) =>
+      fs.filter((f) => f.id !== user.id).concat(user)
+    );
+    return false;
+  });
+};
+
+const renderUser = (user: User) =>
+  DIV(
+    "user",
+    events(DIV("username", user.username), (add) =>
+      add("click", () => {
+        assign("lichess/opponent", user);
+        assign("screen", "challenge");
+      })
+    )
+  );
+
+export const mountFollowing = (root: HTMLElement) => {
+  getFollowing();
+  const users = DIV("users", ...get("lichess/following").map(renderUser));
+  const header = DIV("header", DIV("title", `Following`));
+  root.append(header, users);
+
+  subscribe("lichess/following")(() => {
+    replaceNodeContent(users)(...get("lichess/following").map(renderUser));
+  });
+};
