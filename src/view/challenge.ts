5e92ba6daac94bdb431b48a0e181ed3f956ad23b
Pierre Marchand <pierre.m@atelier-cartographique.be>
2025-06-09 23:36:48 +0200

    style

--- src/view/challenge.ts
+++ src/view/challenge.ts
@@ -1,24 +1,49 @@
 import { challengeUser } from "../api";
 import { events } from "../lib/dom";
 import { DIV, replaceNodeContent } from "../lib/html";
+import { RequesChallengeCreate } from "../lib/ucui/lichess-types";
 import { assign, get, subscribe } from "../store";
 
 const timeControls = [10, 20, 30, 40, 60];
 
-const renderChallenge = (username: string, tc: number) =>
-  events(DIV("challenge-create", tc), (add) =>
+const challenge = (
+  tc: number,
+  color: RequesChallengeCreate["color"],
+  rated: boolean
+): RequesChallengeCreate => ({
+  color,
+  "clock.increment": 0,
+  "clock.limit": tc * 60,
+  keepAliveStream: false,
+  rated,
+  variant: "standard",
+});
+
+const button = (
+  username: string,
+  tc: number,
+  color: RequesChallengeCreate["color"],
+  rated: boolean
+) =>
+  events(DIV(`button button-create ${color} ${rated}`, color), (add) =>
     add("click", () => {
-      challengeUser(username, {
-        color: "random",
-        "clock.increment": 0,
-        "clock.limit": tc * 60,
-        keepAliveStream: false,
-        rated: false,
-        variant: "standard",
-      }).then((challenge) => assign("lichess/my-challenge", challenge));
+      challengeUser(username, challenge(tc, color, rated)).then((challenge) =>
+        assign("lichess/my-challenge", challenge)
+      );
     })
   );
 
+const renderChallenge = (username: string, tc: number) =>
+  DIV(
+    "challenge-create",
+    DIV("time", tc),
+    DIV(
+      "actions",
+      button(username, tc, "black", false),
+      button(username, tc, "random", false),
+      button(username, tc, "white", false)
+    )
+  );
 export const mountChallenge = (root: HTMLElement) => {
   const opponent = get("lichess/opponent");
   if (opponent) {
